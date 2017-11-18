730c5ebb43bc59b7ec9a921f0d25801d921b46ab
lenmorld <lenmorld@gmail.com>
2017-11-18 17:57:14 -0500

    calculating distance

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 /* CORS */
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // var cors = require('cors');
 // app.use(cors());
 
