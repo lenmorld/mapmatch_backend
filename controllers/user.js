ce7d586853580efa27a884d96f0f889359b22e31
lenmorld <lenmorld@gmail.com>
2017-11-18 04:04:48 -0500

    signup

--- controllers/user.js
+++ controllers/user.js
@@ -20,12 +20,15 @@ router.get('/all', function(req, res) {
 
 router.post('/signup', function(req, res) {
   var collection = db.get().collection('users');
+  console.log(req);
+  console.log(res);
 
   collection.insert(req.body, function(err, result) {
     if (err)
       res.json({"message": "Error"});
-    else
-      res.json({"message": "Success", "datasent": res.body});
+    else {
+      res.json({"message": "Success", "datasent": res});
+    }
   });
 
 });
