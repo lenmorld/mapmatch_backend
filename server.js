23cefd8f63a86a59d4ca685c382192122173c8eb
lenmorld <lenmorld@gmail.com>
2017-11-18 03:46:31 -0500

    added CORS on express

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ var db = require('./db');
 app.use(bodyParser.json());
 
 /* CORS */
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 var cors = require('cors');
-
 app.use(cors());
 
 app.use('/users', require('./controllers/user'));
