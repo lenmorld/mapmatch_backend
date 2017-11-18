3323353c221ecec6e707992eb855c49c8e2d8fc9
lenmorld <lenmorld@gmail.com>
2017-11-18 16:52:41 -0500

    calculating distance

--- controllers/user.js
+++ controllers/user.js
@@ -120,7 +120,7 @@ function isNearbyRecursive(destUsers,source,nearbyUsers, ctr){
         ctr++;    // this one processed
 
         console.log("nearby:", nearbyUsers);
-        console.log("c: ",c);
+        console.log("c: ",ctr);
         if (ctr < destUsers.length) {
           isNearbyRecursive(destUsers, source, nearbyUsers, ctr);
         }
